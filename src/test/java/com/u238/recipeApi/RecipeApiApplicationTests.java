881ec140881c062647130453999d5cfb405cb1ca
Oleksii Bilous <bilouole@fit.cvut.cz>
2024-02-11 18:17:26 +0100

    Initial commit

---
+++ src/test/java/com/u238/recipeApi/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u238.recipeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
