34d5b56a367ac0abe3e3abed0265774a3371608b
Bilous Oleksii <bilouole@fit.cvut.cz>
2024-07-10 15:48:38 +0200

    Author rework bug fix, add env.properties to gitignore

--- src/main/java/com/u238/recipeApi/controller/AuthorController.java
+++ src/main/java/com/u238/recipeApi/controller/AuthorController.java
@@ -1,8 +1,10 @@
 package com.u238.recipeApi.controller;
 
 import com.u238.recipeApi.dto.AuthorDto;
+import com.u238.recipeApi.service.AuthorService;
 import com.u238.recipeApi.service.CrudService;
 import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -14,25 +16,10 @@ import java.util.Collection;
 //todo add logging
 @RestController
 @RequestMapping("/author")
+@RequiredArgsConstructor
 public class AuthorController {
 
-    private final CrudService<AuthorDto> service;
-
-    @Autowired
-    public AuthorController(@Qualifier("authorService") CrudService<AuthorDto> service) {
-        this.service = service;
-    }
-
-    @PostMapping
-    public AuthorDto create(@RequestBody AuthorDto authorDto){
-        try {
-            return service.create(authorDto);
-        }catch (ConstraintViolationException e){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }catch (IllegalStateException e){
-            throw new ResponseStatusException(HttpStatus.CONFLICT);
-        }
-    }
+    private final AuthorService service;
 
     @GetMapping("/{id}")
     public AuthorDto read(@PathVariable Long id){
